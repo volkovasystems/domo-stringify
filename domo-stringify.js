c338245dec3c58b04428c06d34054835bda4bade
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2014-03-03 15:55:32 +0800

    Added domoStringify procedure.

---
+++ domo-stringify.js
@@ -0,0 +1,22 @@
+try{ var base = window; }catch( error ){ var base = exports; }
+( function module( base ){
+	define( "domoStringify",
+		[
+			"domo"
+		],
+		function construct( ){
+			var domoStringify = function domoStringify( DOMs ){
+				if( !( DOMs instanceof Array ) ){
+					var parameters = Array.prototype.slice.apply( arguments );
+					DOMs = parameters;
+				}
+				for( var index = 0; index < DOMs.length; index++ ){
+					DOMs[ index ] = DOMs[ index ].outerHTML;
+				}
+				return DOMs.join( "" );
+			};
+
+			base.domoStringify = domoStringify;
+			return domoStringify;
+		} );
+} )( base );
